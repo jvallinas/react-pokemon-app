87f119be35c8c66ee0732f40030e812346b2a265
Javier <ff_valli@hotmail.com>
2019-11-10 22:50:34 +0100

    lint: override no-unused-vars as warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "jest": true
     },
     "extends": [
         "eslint:recommended",
@@ -24,5 +25,6 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        "no-unused-vars": "warn"
     }
 };
