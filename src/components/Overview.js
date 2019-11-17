b02bfc7a66c8c062f119f5585ac3e9dde9759f2e
Javier <javier.vallinaslima@cognizant.com>
2019-11-18 00:30:33 +0100

    Refactor Overview component to use CSS Modules

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import styles from './Overview.module.css';
 
 import './Overview.css';
 
@@ -9,33 +10,37 @@ function Overview() {
 	}, []);
 
 	let history = useHistory();
-	const handleClick = () => history.push("/selection");
+	const navigateToSelection = () => history.push('/selection');
 
 	return (
-		<div className="Overview">
-			<div className="header-container">
-				<h1 className="header-title">POKEMON BATTLE</h1>
-				<div className="subtitle">Test your skills building a Pokemon application</div>
-				<button className="go-to-selection" onClick={handleClick}>{`LET'S DO IT!`}</button>
+		<>
+			<div className={styles['header-container']}>
+				<h1 className={styles['header-title']}>{props.title.toUpperCase()}</h1>
+				<div className={styles['subtitle']}>{props.subtitle}</div>
+				<button className={styles['go-to-selection']} onClick={navigateToSelection}>{`LET'S DO IT!`}</button>
 			</div>
 
-			<section className="description">
-				<h2 className="section-title">OBJECTIVE</h2>
-				<div className="description">The goal is to create a Pokemon application using the React framework and the Pokemon API.</div>
-
-				<h2 className="section-title">CHALLENGE</h2>
-				<div className="description">
+			<div className={styles['feature-list']}>
+				<section>
+					<h1 className={styles['section-title']}>OBJECTIVE</h1>
+					<div className={styles['description']}>The goal is to create a Pokemon application using the React framework and the Pokemon API.</div>
+				</section>
+				<section>
+					<h1 className={styles['section-title']}>CHALLENGE</h1>
+					<div className={styles['description']}>
 					We will be creating a model to instantiate new pokemons and supply our application with data, having several fields such as their name, type, weakness and other stats.
 				</div>
-
-				<h2 className="section-title">HOW IT WORKS</h2>
+				</section>
+				<section>
+					<h1 className={styles['section-title']}>HOW IT WORKS</h1>
 				<video width="auto" height="auto" controls>
 					(Your browser does not support the video tag).
 				</video>
 			</section>
+			</div>
 
-			<button className="go-to-selection" onClick={handleClick}>{`LET'S DO IT!`}</button>
-		</div>
+			<button className={styles['go-to-selection']} onClick={navigateToSelection}>{`LET'S DO IT!`}</button>
+		</>
 	);
 }
 
