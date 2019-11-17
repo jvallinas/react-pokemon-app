70d4ee64a5971860ab8141425d16963a0624b3c9
Javier <ff_valli@hotmail.com>
2019-11-17 14:45:45 +0100

    Add custom hook useHttpRequest

---
+++ src/hooks/useHttpRequest.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react';
+
+const useHttpRequest = (url, options) => {
+  const [response, setResponse] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRequestActivated, setIsRequestActivated] = useState(true);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(url, options);
+      const json = await res.json();
+      setResponse(json);
+    }
+    catch (error) {
+      setError(error);
+    }
+    setIsLoading(false);
+    setIsRequestActivated(false);
+  };
+
+  useEffect(() => {
+    if (isRequestActivated) {
+      fetchData();
+    }
+  }, [isRequestActivated]);
+
+  const activateRequest = () => setIsRequestActivated(true);
+
+  return { response, error, isLoading, activateRequest };
+};
+
+export default useHttpRequest;
