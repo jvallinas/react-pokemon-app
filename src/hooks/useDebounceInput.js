ee62736beea30180a576cbb3edcea7f8b0e25f74
Javier <javier.vallinaslima@cognizant.com>
2019-11-20 01:16:36 +0100

    Add custom hook for debouncing input

---
+++ src/hooks/useDebounceInput.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+export default function useDebounceInput(input, delay) {
+
+  const [debouncedInput, setDebouncedInput] = useState(input);
+
+  useEffect(
+    () => {
+      // Set debouncedInput only after the specified delay
+      const handler = setTimeout(() => {
+        setDebouncedInput(input);
+      }, delay);
+
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    // Only re-call effect when input changes
+    [input]
+  );
+
+  return debouncedInput;
+}
