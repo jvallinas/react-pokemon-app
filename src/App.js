30fbc52950ea5b211288e1308db4cf7b3f261b30
RS12NT <javier.vallinas@ing.com>
2019-11-21 10:01:53 +0100

    Add routes.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { Provider as ReduxProvider } from "react-redux";
-import configureStore from "./containers/App/store";
-
 import './App.css';
 
+import ROUTES from './routes';
 import Overview from './components/overview/Overview';
 import Selection from './components/selection/Selection';
+
+import { Provider as ReduxProvider } from 'react-redux';
+import configureStore from './containers/App/store';
+
 const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
 
 function App() {
@@ -16,11 +18,11 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Switch>
-            <Route exact path="/">
+            <Route exact path={ROUTES.APP.ROOT}>
               <Overview title={'Pokemon Battle'} subtitle={'Test your skills building a Pokemon application'}/>
             </Route>
-            <Route path="/selection">
-              <Selection title={'Select your Pokemon'} limit={50} offset={20} />                    
+            <Route path={ROUTES.APP.SELECTION}>
+              <Selection title={'Select your Pokemon'} limit={25} offset={0} />                    
             </Route>
           </Switch>
         </BrowserRouter>
