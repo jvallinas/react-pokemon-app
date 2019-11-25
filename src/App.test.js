d98634a99d607af133a4d57ae739201ed1f8da7e
Javier <javier.vallinaslima@cognizant.com>
2019-11-26 00:55:41 +0100

    Add initial setup for testing and basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import { Overview, Selection} from './views'
+
+describe('Basic tests', () => {
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+})
+
+describe('Views rendered for each route', () =>{
+  it('Root path should render Overview component', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(wrapper.find(Overview)).toHaveLength(1);
+    expect(wrapper.find(Selection)).toHaveLength(0);
+  });
+
+  it('Selection path should render Selection component', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/selection']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(wrapper.find(Overview)).toHaveLength(0);
+    expect(wrapper.find(Selection)).toHaveLength(1);
+  });
+
+  it('Invalid path should not show any of the views', () => {
+    const wrapper = mount(
+      <MemoryRouter initialEntries={['/invalid']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(wrapper.find(Overview)).toHaveLength(0);
+    expect(wrapper.find(Selection)).toHaveLength(0);
+  });
+
 });
---
+++ src/components/overview/Overview.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Overview from './Overview';
+import OverviewStep from './elements/OverviewStep';
+
+// Workaround for useHistory since react-router context isn't set in this component
+jest.mock('react-router-dom', () => ({
+	...jest.requireActual('react-router-dom'),
+	useHistory: () => ({
+		push: jest.fn()
+	})
+}));
+
+const mockTitle = 'Welcome to React Pokemon app';
+const mockSubtitle = 'An app for learning React';
+
+describe('Layout rendering in Overview page', () => {
+
+	it('Renders correct number of button elements', () => {
+		const wrapper = shallow(<Overview title={mockTitle} subtitle={mockSubtitle} />);
+		expect(wrapper.find('button').length).toEqual(2);
+	});
+
+	it('Renders correct number of Step items', () => {
+		const wrapper = shallow(<Overview title={mockTitle} subtitle={mockSubtitle} />);
+		expect(wrapper.find(OverviewStep).length).toEqual(3);
+	});
+});
+
