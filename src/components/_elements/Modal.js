8a9457fd02b72a2ae8112b8b56dfe3428e270ae6
Javier <javier.vallinaslima@cognizant.com>
2019-11-27 23:41:48 +0100

    Added placeholder modal dialog component

---
+++ src/components/_elements/Modal.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Modal.module.css';
+
+const modalPropTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+  onModalClose: PropTypes.func,
+  show: PropTypes.bool
+}
+
+// TODO improve accessibility features or find a modal library
+const Modal = ({ onModalClose, show, children }) => {
+  const modalStatusClass = show ? 'modal-visible' : 'modal-hidden';
+
+  return (
+    <div className={styles[modalStatusClass] }>
+      <section className={styles['modal-frame']}>
+        {children}
+        <button className={styles.close}
+          onClick={onModalClose}
+        >
+          Close
+        </button>
+      </section>
+    </div>
+  );
+};
+
+Modal.propTypes = modalPropTypes;
+
+export default Modal;
